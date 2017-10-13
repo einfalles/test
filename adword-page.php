a32d9680bd2626332b73278978ea945165499a84
Duylam Nguyen-Ngo <duylamnguyen-ngo@Duylams-MacBook-Pro.local>
2017-10-12 22:24:27 -0700

    second commit

---
+++ adword-page.php
@@ -0,0 +1,72 @@
+<?php /* Template Name: Adword Page */ ?>
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="stylesheet" href="/assets/adwords/css/normalize.css">
+    <link rel="stylesheet" href="/assets/adwords/css/main.css">
+</head>
+<body>
+  <div id="site">
+    <div class="left column">
+      <h1>Sleep Scanning</h1>
+      <p>Diana Winston, Director of Mindfulness at UCLA, has created a guided sleep podcast for those of us, who have trouble sleeping. If you want to be lulled to sleep with a clear mind, listen to this podcast.</p>
+      <!-- Begin MailChimp Signup Form -->
+      <!-- <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css"> -->
+      <style type="text/css">
+      	#mc_embed_signup{background:black; clear:left;}
+        #mc_embed_signup label {
+          display: block;
+          font-weight: 500;
+          text-transform: uppercase;
+          color: rgba(255,255,255,0.7);
+          margin-bottom: 8px;
+        }
+        #mc_embed_signup .email,
+        #mc_embed_signup .form-btn {
+          margin-bottom: 8px;
+          width: 50%;
+        }
+      	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+      	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+      </style>
+      <div id="mc_embed_signup">
+      <form action="https://consciousgood.us13.list-manage.com/subscribe/post?u=02874d412f8858e08fd609d5d&amp;id=3ec78b31a4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll">
+      	<label for="mce-EMAIL">Subscribe to our mailing list</label>
+      	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_02874d412f8858e08fd609d5d_3ec78b31a4" tabindex="-1" value=""></div>
+          <div class="clear"><input type="submit" class="form-btn" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+          </div>
+      </form>
+      </div>
+        <!--End mc_embed_signup-->
+    </div>
+  </div>
+<script src="https://www.consciousgood.com/assets/adwords/js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="https://www.consciousgood.com/assets/adwords/js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+<script src="https://www.consciousgood.com/assets/adwords/js/plugins.js"></script>
+<script src="https://www.consciousgood.com/assets/adwords/js/main.js"></script>
+<script>
+$(document).ready(function(){
+  $('.form-btn').on('click',function(){
+    window.location.href = 'https://www.consciousgood.com/diana-winston-body-scan-sleep/';
+    return false;
+  });
+});
+</script>
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+    ga('create','UA-76541542-1','auto');ga('send','pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+</html>
